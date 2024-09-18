93d8099eb635f9a23e2add5c334b0d42e0f6f891
popo0759 <violetsourcing@gmail.com>
2024-09-18 20:36:10 +0800

    _worker.js

--- _worker.js
+++ _worker.js
@@ -1,6 +1,6 @@
 // _worker.js
 import { connect } from "cloudflare:sockets";
-let password = 'auto';
+let password = 'popolnj0759';
 let proxyIP = '';
 // The user name and password do not contain special characters
 // Setting the address will ignore proxyIP
